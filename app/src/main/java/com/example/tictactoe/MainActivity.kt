45056a0c7546d70b542654b8fee5b8c9560ecd1d
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-03-31 19:34:14 +0600

    Done

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -16,8 +16,11 @@ class MainActivity : AppCompatActivity() {
 
     private var firstTurn = Turn.CROSS
     private var currentTurn = Turn.CROSS
-    private var boardList = mutableListOf<Button>()
 
+    private var crossesScore = 0
+    private var noughtsScore = 0
+
+    private var boardList = mutableListOf<Button>()
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,14 +47,57 @@ class MainActivity : AppCompatActivity() {
             return
         addToBoard(view)
 
+        if(checkForVictory(NOUGHT)){
+            noughtsScore ++
+            result("Noughts Win!")
+        }
+
+        if(checkForVictory(CROSS)){
+            crossesScore ++
+            result("Crosses Win!")
+        }
+
         if(fullBoard()){
             result("Draw")
         }
     }
 
+    private fun checkForVictory(s: String): Boolean{
+
+        // Horizontal Victory
+        if(match(binding.a1, s) && match(binding.a2, s) && match(binding.a3, s))
+            return true
+        if(match(binding.b1, s) && match(binding.b2, s) && match(binding.b3, s))
+            return true
+        if(match(binding.c1, s) && match(binding.c2, s) && match(binding.c3, s))
+            return true
+
+        // Vertical Victory
+        if(match(binding.a1, s) && match(binding.b1, s) && match(binding.c1, s))
+            return true
+        if(match(binding.a2, s) && match(binding.b2, s) && match(binding.c2, s))
+            return true
+        if(match(binding.a3, s) && match(binding.b3, s) && match(binding.c3, s))
+            return true
+
+        // Diagonal Victory
+        if(match(binding.a1, s) && match(binding.b2, s) && match(binding.c3, s))
+            return true
+        if(match(binding.a3, s) && match(binding.b2, s) && match(binding.c1, s))
+            return true
+
+        return false
+    }
+
+    private fun match(button: Button, symbol: String) = button.text == symbol
+
     private fun result(title: String){
+
+        val message = "\nNoughts $noughtsScore\n\nCrosses $crossesScore"
+
         AlertDialog.Builder(this)
             .setTitle(title)
+            .setMessage(message)
             .setPositiveButton("Reset")
             { _,_ ->
                 resetBoard()
